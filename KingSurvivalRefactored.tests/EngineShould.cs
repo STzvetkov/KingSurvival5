1f69cd4550207d9bf1ce30e2eeebdde134e7c648
georgikostadinov <georgikostadinov@yahoo.com>
2014-07-24 22:55:29 +0300

    Started unit testing the Engine, but all its methods are private...

---
+++ KingSurvivalRefactored.tests/EngineShould.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KingSurvivalRefactored.tests
+{
+    [TestClass]
+    public class EngineShould
+    {
+        Engine engine;
+        Figure[] figures;
+        Figure pawnA;
+        Figure pawnB;
+        Figure king;
+        [ClassInitialize]
+        public void ClassInitialize() 
+        {
+            
+            this.engine = new Engine();
+            this.pawnA = new Pawn(new FieldCell(1, 1, ' ', ConsoleColor.Red), 'A');
+            this.pawnB = new Pawn(new FieldCell(2,2, ' ', ConsoleColor.Black), 'B');
+            this.king = new King(new FieldCell(3, 3, ' ', ConsoleColor.Cyan), 'C');
+            this.figures = new Figure[3]{pawnA,pawnB,king};
+
+        }
+
+        [TestMethod]
+        public void TestExtractRequestedFigure()
+        {
+            string input = "ADR";
+            
+        }
+    }
+}
