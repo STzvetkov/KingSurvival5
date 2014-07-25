e66aa9155fc08734251a5c8eb1f3dfe7511fb790
TanyaDimitrova <tanya.dimitrova@mail.bg>
2014-07-25 23:30:02 +0300

    A few more tests added to Checker for 100% coverage

--- KingSurvivalRefactored.tests/CheckerTest.cs
+++ KingSurvivalRefactored.tests/CheckerTest.cs
@@ -10,84 +10,163 @@ namespace KingSurvivalRefactored.tests
         [TestMethod]
         public void IsValidMoveForKingShouldReturnTrueForCorrectData()
         {
-            IFigure king = new King(new FieldCell(2, 3, 'K', ConsoleColor.Blue), 'K');
+            IFigure king = this.InitKing(2, 3);
             bool isValid = Checker.Instance.IsValidMove(king, "kur");
-            Assert.IsTrue(isValid,"KUR should be valid move for king");
+            Assert.IsTrue(isValid, "KUR should be valid move for king");
         }
 
         [TestMethod]
         public void IsValidMoveForKingShouldReturnFalseForIvalidDirection()
         {
-            IFigure king = new King(new FieldCell(2, 3, 'K', ConsoleColor.Blue), 'K');
+            IFigure king = this.InitKing(2, 3);
             bool isValid = Checker.Instance.IsValidMove(king, "kfr");
-            Assert.IsFalse(isValid,"KFR shouldn't be valid move");
+            Assert.IsFalse(isValid, "KFR shouldn't be valid move");
         }
 
         [TestMethod]
         public void HasKingWonShouldReturtTrueWhenTheKingIsOnTheTop()
         {
-            IFigure king = new King(new FieldCell(0, 0, 'K', ConsoleColor.Blue), 'K');
+            IFigure king = this.InitKing(0, 0);
             bool hasWon = Checker.Instance.HasKingWon(king as King);
-            Assert.IsTrue(hasWon,"If the king is with Y position = 0, it should win");
+            Assert.IsTrue(hasWon, "If the king is with Y position = 0, it should win");
         }
 
         [TestMethod]
         public void HasKingWonShouldReturtFalseWhenTheKingIsNotOnTheTop()
         {
-            IFigure king = new King(new FieldCell(0, 1, 'K', ConsoleColor.Blue), 'K');
+            IFigure king = this.InitKing(0, 1);
             bool hasWon = Checker.Instance.HasKingWon(king as King);
             Assert.IsFalse(hasWon, "If the king is NOT with Y position = 0, it should NOT win");
         }
 
         [TestMethod]
-        public void HasKingLostShouldReturnFalseWhenTheCellsAroundTheKingAreFree()
+        public void HasKingLostShouldReturnFalseWhenAllTheCellsAroundTheKingAreFree()
         {
-            IFigure king = new King(new FieldCell(8, 3, 'K', ConsoleColor.Blue), 'K');
-            Table table = new Table(new FieldCellFactory(9, 9, ' ', ConsoleColor.Red, ConsoleColor.DarkCyan), new Frame("test.txt"));
+            IFigure king = this.InitKing(5, 5);
+            Table table = this.InitTable(9, 9);
             bool hasLost = Checker.Instance.HasKingLost(king as King, table);
-            Assert.IsFalse(hasLost,"The king cannot lose if there is a free cell around it");
+            Assert.IsFalse(hasLost, "The king cannot lose if there is a free cell around it");
+        }
+
+        [TestMethod]
+        public void HasKingLostShouldReturnFalseWhenBottomRightCellsAroundTheKingIsFree()
+        {
+            IFigure king = this.InitKing(5, 5);
+            Table table = this.InitTable(9, 9);
+
+            table.Cells[4, 4] = new FieldCell(4, 4, 'A', ConsoleColor.Blue);
+            table.Cells[4, 6] = new FieldCell(4, 6, 'B', ConsoleColor.Blue);
+            table.Cells[6, 4] = new FieldCell(6, 4, 'C', ConsoleColor.Blue);
+
+            bool hasLost = Checker.Instance.HasKingLost(king as King, table);
+            Assert.IsFalse(hasLost, "The king cannot lose if there is a free cell around it");
+        }
+
+        [TestMethod]
+        public void HasKingLostShouldReturnFalseWhenBottomLeftCellsAroundTheKingIsFree()
+        {
+            IFigure king = this.InitKing(5, 5);
+            Table table = this.InitTable(9, 9);
+
+            table.Cells[4, 4] = new FieldCell(4, 4, 'A', ConsoleColor.Blue);
+            table.Cells[6, 4] = new FieldCell(6, 4, 'B', ConsoleColor.Blue);
+            table.Cells[6, 6] = new FieldCell(6, 6, 'D', ConsoleColor.Blue);
+
+            bool hasLost = Checker.Instance.HasKingLost(king as King, table);
+            Assert.IsFalse(hasLost, "The king cannot lose if there is a free cell around it");
+        }
+
+        [TestMethod]
+        public void HasKingLostShouldReturnFalseWhenUpperLeftCellsAroundTheKingIsFree()
+        {
+            IFigure king = this.InitKing(5, 5);
+            Table table = this.InitTable(9, 9);
+
+            table.Cells[6, 4] = new FieldCell(6, 4, 'B', ConsoleColor.Blue);
+            table.Cells[4, 6] = new FieldCell(4, 6, 'B', ConsoleColor.Blue);
+            table.Cells[6, 6] = new FieldCell(6, 6, 'D', ConsoleColor.Blue);
+
+            bool hasLost = Checker.Instance.HasKingLost(king as King, table);
+            Assert.IsFalse(hasLost, "The king cannot lose if there is a free cell around it");
+        }
+
+        [TestMethod]
+        public void HasKingLostShouldReturnFalseWhenUpperRightCellsAroundTheKingIsFree()
+        {
+            IFigure king = this.InitKing(5, 5);
+            Table table = this.InitTable(9, 9);
+
+            table.Cells[4, 4] = new FieldCell(4, 4, 'a', ConsoleColor.Blue);
+            table.Cells[4, 6] = new FieldCell(4, 6, 'B', ConsoleColor.Blue);
+            table.Cells[6, 6] = new FieldCell(6, 6, 'D', ConsoleColor.Blue);
+
+            bool hasLost = Checker.Instance.HasKingLost(king as King, table);
+            Assert.IsFalse(hasLost, "The king cannot lose if there is a free cell around it");
+        }
+
+        [TestMethod]
+        public void HasKingLostShouldReturnFalseWhenThereIsPossibleMoveAndKingIsAtTheEndOfTheTable()
+        {
+            IFigure king = this.InitKing(8, 8);
+            Table table = this.InitTable(9, 9);
+            table.Cells[4, 4] = new FieldCell(7, 7, 'a', ConsoleColor.Blue);
+            table.Cells[4, 6] = new FieldCell(4, 6, 'B', ConsoleColor.Blue);
+            table.Cells[6, 6] = new FieldCell(6, 6, 'D', ConsoleColor.Blue);
+
+            bool hasLost = Checker.Instance.HasKingLost(king as King, table);
+            Assert.IsFalse(hasLost, "The king cannot lose if there is a free cell around it");
+        }
+
+        [TestMethod]
+        public void HasKingLostShouldReturnFalseWhenThereIsPossibleMoveAndKingIsAtTheStartOfTheTable()
+        {
+            IFigure king = this.InitKing(0, 0);
+            Table table = this.InitTable(9, 9);
+            table.Cells[4, 4] = new FieldCell(4, 4, 'a', ConsoleColor.Blue);
+            table.Cells[4, 6] = new FieldCell(4, 6, 'B', ConsoleColor.Blue);
+            table.Cells[6, 6] = new FieldCell(6, 6, 'D', ConsoleColor.Blue);
+
+            bool hasLost = Checker.Instance.HasKingLost(king as King, table);
+            Assert.IsFalse(hasLost, "The king cannot lose if there is a free cell around it");
+        }
+
+        [TestMethod]
+        public void HasKingLostShouldReturnTrue()
+        {
+            IFigure king = this.InitKing(5, 5);
+            Table table = this.InitTable(9, 9);
+
+            table.Cells[4, 4] = new FieldCell(4, 4, 'A', ConsoleColor.Blue);
+            table.Cells[4, 6] = new FieldCell(4, 6, 'B', ConsoleColor.Blue);
+            table.Cells[6, 4] = new FieldCell(6, 4, 'C', ConsoleColor.Blue);
+            table.Cells[6, 6] = new FieldCell(6, 6, 'D', ConsoleColor.Blue);
+
+            bool hasLost = Checker.Instance.HasKingLost(king as King, table);
+            Assert.IsTrue(hasLost, "The king lose if there is no  a free cell around it");
         }
 
         [TestMethod]
         public void IsValidFigureRequestedWithCorrectDataInPawnsTurn()
         {
-            IFigure[] figures = new IFigure[]
-            {
-                new King(new FieldCell(8, 1, 'K', ConsoleColor.Yellow), 'K'),
-                new Pawn(new FieldCell(8, 3, 'A', ConsoleColor.Blue), 'A'),
-                new Pawn(new FieldCell(8, 6, 'B', ConsoleColor.Blue), 'B'),
-                new Pawn(new FieldCell(8, 1, 'C', ConsoleColor.Blue), 'C')
-            };
+            IFigure[] figures = this.GetFigures();
 
             bool isValid = Checker.Instance.IsValidFigureRequested(2, "cdl", figures);
-            Assert.IsTrue(isValid,"C should be valid figure when its pawn's turn");
+            Assert.IsTrue(isValid, "C should be valid figure when its pawn's turn");
         }
 
         [TestMethod]
         public void IsValidFigureRequestedWithIncorrectFigureRequestedInPawnsTurn()
         {
-            IFigure[] figures = new IFigure[]
-            {
-                new King(new FieldCell(8, 1, 'K', ConsoleColor.Yellow), 'K'),
-                new Pawn(new FieldCell(8, 3, 'A', ConsoleColor.Blue), 'A'),
-                new Pawn(new FieldCell(8, 6, 'B', ConsoleColor.Blue), 'B'),
-                new Pawn(new FieldCell(8, 1, 'C', ConsoleColor.Blue), 'C')
-            };
+            IFigure[] figures = this.GetFigures();
 
             bool isValid = Checker.Instance.IsValidFigureRequested(2, "kdl", figures);
-            Assert.IsFalse(isValid,"K should not be valid figure when its pawn's turn");
+            Assert.IsFalse(isValid, "K should not be valid figure when its pawn's turn");
         }
 
         [TestMethod]
         public void IsValidFigureRequestedWithInorrectDataInKingsTurn()
         {
-            IFigure[] figures = new IFigure[]
-            {
-                new King(new FieldCell(8, 1, 'K', ConsoleColor.Yellow), 'K'),
-                new Pawn(new FieldCell(8, 3, 'A', ConsoleColor.Blue), 'A'),
-                new Pawn(new FieldCell(8, 6, 'B', ConsoleColor.Blue), 'B'),
-                new Pawn(new FieldCell(8, 1, 'C', ConsoleColor.Blue), 'C')
-            };
+            IFigure[] figures = this.GetFigures();
 
             bool isValid = Checker.Instance.IsValidFigureRequested(3, "cdl", figures);
             Assert.IsFalse(isValid, "C should not be valid figure when its king's turn");
@@ -96,13 +175,7 @@ namespace KingSurvivalRefactored.tests
         [TestMethod]
         public void IsValidFigureRequestedWithCorrectFigureRequestedInKingsTurn()
         {
-            IFigure[] figures = new IFigure[]
-            {
-                new King(new FieldCell(8, 1, 'K', ConsoleColor.Yellow), 'K'),
-                new Pawn(new FieldCell(8, 3, 'A', ConsoleColor.Blue), 'A'),
-                new Pawn(new FieldCell(8, 6, 'B', ConsoleColor.Blue), 'B'),
-                new Pawn(new FieldCell(8, 1, 'C', ConsoleColor.Blue), 'C')
-            };
+            IFigure[] figures = this.GetFigures();
 
             bool isValid = Checker.Instance.IsValidFigureRequested(3, "kdl", figures);
             Assert.IsTrue(isValid, "K should be valid figure when its king's turn");
@@ -111,7 +184,7 @@ namespace KingSurvivalRefactored.tests
         [TestMethod]
         public void IsRequestedPositionInsideTableShouldReturnTrueForCorrectPosition()
         {
-            Table table = new Table(new FieldCellFactory(9, 9, ' ', ConsoleColor.Red, ConsoleColor.DarkCyan), new Frame("test.txt"));
+            Table table = this.InitTable(9, 9);
             bool result = Checker.Instance.IsRequestedPositionInsideTable(3, 3, table);
             Assert.IsTrue(result);
         }
@@ -119,9 +192,33 @@ namespace KingSurvivalRefactored.tests
         [TestMethod]
         public void IsRequestedPositionInsideTableShouldReturnFalseForIncorrectPosition()
         {
-            Table table = new Table(new FieldCellFactory(9, 9, ' ', ConsoleColor.Red, ConsoleColor.DarkCyan), new Frame("test.txt"));
+            Table table = this.InitTable(9, 9);
             bool result = Checker.Instance.IsRequestedPositionInsideTable(13, 23, table);
             Assert.IsFalse(result);
+        }
+
+        private Table InitTable(int rowsCountInTable, int colsCountInTable)
+        {
+            return new Table(new FieldCellFactory(rowsCountInTable, colsCountInTable, ' ',
+                ConsoleColor.Red, ConsoleColor.DarkCyan), new Frame("test.txt"));
+        }
+
+        private King InitKing(int kingRow, int kingCol)
+        {
+            return new King(new FieldCell(kingRow, kingCol, 'K', ConsoleColor.Blue), 'K');
+        }
+        
+        private IFigure[] GetFigures()
+        {
+            IFigure[] figures = new IFigure[]
+            {
+                new King(new FieldCell(8, 1, 'K', ConsoleColor.Yellow), 'K'),
+                new Pawn(new FieldCell(8, 3, 'A', ConsoleColor.Blue), 'A'),
+                new Pawn(new FieldCell(8, 6, 'B', ConsoleColor.Blue), 'B'),
+                new Pawn(new FieldCell(8, 1, 'C', ConsoleColor.Blue), 'C')
+            };
+
+            return figures;
         }
     }
 }
